9e4aca83daec2770f3cd24780b225dee1f6d1e69
Namratha Basavanahalli Manjunatha <namratha@rita.cat.pdx.edu>
2018-02-01 19:47:05 -0800

    04 05 commits

--- 04-convolution.py
+++ 04-convolution.py
@@ -29,39 +29,26 @@ numpyOutput = numpy.zeros([numpyInput.shape[0], numpyInput.shape[1], 3], numpy.f
 
 # since this can be tricky and you might not be perfectly familiar with indexing and splicing matrices yet, this is already done for you below
 
-blue = numpyInput[1::2, 1::2]
-green1 = numpyInput[0::2, 1::2]
-green2 = numpyInput[1::2, 0::2]
-red = numpyInput[0::2, 0::2]
-
-#print("numpytInput")
-#print(numpyInput)
-#print("RED: ")
-#print(red)
-
-kernelBlue = [ [0.25,0.50,0.25],[0.50,1,0.50],[0.25,0.50,0.25] ]
-kernelGreen = [ [0.00,0.25,0.00],[0.25,1,0.25],[0.00,0.25,0.00] ]  
-kernelRed = [ [0.25,0.50,0.25],[0.50,1,0.50],[0.25,0.50,0.25] ]
-
-convBlue = cv2.filter2D(blue,-1,numpy.asarray(kernelBlue),cv2.BORDER_DEFAULT)
-convGreen1 = cv2.filter2D(green1,-1,numpy.asarray(kernelGreen),cv2.BORDER_DEFAULT)
-convGreen2 = cv2.filter2D(green2,-1,numpy.asarray(kernelGreen),cv2.BORDER_DEFAULT)
-convRed = cv2.filter2D(red,-1,numpy.asarray(kernelRed),cv2.BORDER_DEFAULT)
-print (kernelBlue)
-numpyOutput[1::2, 1::2, 0] = convBlue
-numpyOutput[0::2, 1::2, 1] = convGreen1
-numpyOutput[1::2, 0::2, 1] = convGreen2
-numpyOutput[0::2, 0::2, 2] = convRed
+numpyOutput[1::2,1::2,0] = numpyInput[1::2, 1::2]
+numpyOutput[0::2,1::2,1] = numpyInput[0::2, 1::2]
+numpyOutput[1::2,0::2,1] = numpyInput[1::2, 0::2]
+numpyOutput[0::2,0::2,2] = numpyInput[0::2, 0::2]
 
 # for each channel in numpyOutput, use a suitable convolution to perform the demosaicing and store the output back in its respective channel in numpyOutput
 # we already discussed in class what the appropriate kernel for the green channel is, determining the kernel for the other two channels is up to you
 # to be able to convolve a channel from numpyOutput and storing the result back in the same channel, the convolution must not affect the resolution
 # we need padding for the convolution to not affect the resolution, this is already built into OpenCV but make sure to use cv2.BORDER_DEFAULT as the border type
 
+kernelBlue = [ [0.25,0.5,0.25],[0.5,1,0.5],[0.25,0.5,0.25] ]
+kernelGreen = [ [0.00,0.25,0.00],[0.25,1,0.25],[0.00,0.25,0.00] ]
+kernelRed = [ [0.25,0.5,0.25],[0.5,1,0.5],[0.25,0.5,0.25] ]
 
-#print(numpyInput.shape)
-#print(numpyOutput.shape)
+numpykernelBlue=numpy.array(kernelBlue,numpy.float32)
+numpykernelGreen=numpy.array(kernelGreen,numpy.float32)
+numpykernelRed=numpy.array(kernelRed,numpy.float32)
 
-
+numpyOutput[:,:,0] = cv2.filter2D(numpyOutput[:,:,0],-1,numpykernelBlue,cv2.BORDER_DEFAULT)
+numpyOutput[:,:,1] = cv2.filter2D(numpyOutput[:,:,1],-1,numpykernelGreen,cv2.BORDER_DEFAULT)
+numpyOutput[:,:,2] = cv2.filter2D(numpyOutput[:,:,2],-1,numpykernelRed,cv2.BORDER_DEFAULT)
 
 cv2.imwrite(filename='./04-convolution.png', img=(numpyOutput * 255.0).clip(0.0, 255.0).astype(numpy.uint8))
